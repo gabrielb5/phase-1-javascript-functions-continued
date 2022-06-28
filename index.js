495d3c3c215a6cade520c9d9193db9094e19a1c5
Gabriel Burgos <gabrielburgosmm@gmail.com>
2022-06-28 09:53:37 -0700

    commit tree 6b6469ab39f8680f7214f11653a60727775669fa

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+ function saturdayFun(event = "roller-skate") {
+    return `This Saturday, I want to ${event}!`
+ }
+
+ function mondayWork(event = "go to the office") {
+    return `This Monday, I will ${event}.`
+ }
+
+const wrapAdjective = function(style = "*") {
+ return function(adjective = "special")  {
+ return `You are ${style}${adjective}${style}!`} 
+ } 
+ 
